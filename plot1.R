37976f693b70c5d945bde423d3da3706baefaafe
riccozero <riccozero@gmail.com>
2014-05-19 00:03:56 -0500

    Question 1, Revision 1
    No log-scale

--- plot1.R
+++ plot1.R
@@ -1,9 +1,9 @@
 NEI <- readRDS("summarySCC_PM25.rds")
-NEI <- transform(NEI, logEmissions=log10(Emissions), year=as.factor(year))
+NEI <- transform(NEI, year=as.factor(year))
 png("plot1.png", width=640, height=640)
 
-   boxplot(logEmissions ~ year, data=NEI, ylim=c(-7.5,5))
-   title(ylab = expression(log[10]*" "*PM[2.5]),
+   boxplot(Emissions ~ year, data=NEI, ylim=c(-0.5,1))
+   title(ylab = expression(PM[2.5]),
          main = expression("Total "*PM[2.5]*" Emission by Year"))
 
 dev.off()
