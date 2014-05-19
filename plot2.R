b61229b935f9b2ac132520d2027a35e331cc4e9d
riccozero <riccozero@gmail.com>
2014-05-19 00:10:43 -0500

    Question 2 Revision 2
    Fixed png file name

--- plot2.R
+++ plot2.R
@@ -6,7 +6,7 @@ NEI <- transform(NEI, year=as.factor(year))
 ### Question 2
 
 NEIBaltimore <- NEI[NEI$fip=="24510",]
-png("plot2b.png", width=640, height=640)
+png("plot2.png", width=640, height=640)
 
    boxplot(Emissions ~ year, data=NEIBaltimore, ylim=c(-0,2))
    title(ylab = expression(PM[2.5]),
