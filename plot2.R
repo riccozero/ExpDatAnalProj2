f30b985e9ca64839212e44b74d48728bf3be5c48
riccozero <riccozero@gmail.com>
2014-05-19 00:08:58 -0500

    Question 2 Revision 1
    No log scale

--- plot2.R
+++ plot2.R
@@ -1,24 +1,15 @@
 NEI <- readRDS("summarySCC_PM25.rds")
-NEI <- transform(NEI, logEmissions=log10(Emissions), year=as.factor(year))
+NEI <- transform(NEI, year=as.factor(year))
 
 
-### Question 1
-
-# png("plot1.png", width=640, height=640)
-#
-#   boxplot(logEmissions ~ year, data=NEI, ylim=c(-7.5,5))
-#   title(ylab = expression(log[10]*" "*PM[2.5]),
-#         main = expression("Total "*PM[2.5]*" Emission by Year"))
-#
-# dev.off()
 
 ### Question 2
 
 NEIBaltimore <- NEI[NEI$fip=="24510",]
-png("plot2.png", width=640, height=640)
+png("plot2b.png", width=640, height=640)
 
-   boxplot(logEmissions ~ year, data=NEIBaltimore, ylim=c(-7.5,5))
-   title(ylab = expression(log[10]*" "*PM[2.5]),
+   boxplot(Emissions ~ year, data=NEIBaltimore, ylim=c(-0,2))
+   title(ylab = expression(PM[2.5]),
          main = expression("Total "*PM[2.5]*" Emission by Year in Baltimore"))
 
 dev.off()
