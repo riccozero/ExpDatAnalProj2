89549e79001f995f361c68c38a8daf47e61554ef
riccozero <riccozero@gmail.com>
2014-05-19 22:38:06 -0500

    Question 1 Final
    Fixing ylim

--- plot1.R
+++ plot1.R
@@ -1,8 +1,9 @@
 NEI <- readRDS("summarySCC_PM25.rds")
 NEI <- transform(NEI, year=as.factor(year))
-png("plot1.png", width=640, height=640)
 
-   boxplot(Emissions ~ year, data=NEI, ylim=c(-0.5,1))
+png("plot1.png", width=640, height=480)
+
+   boxplot(Emissions ~ year, data=NEI, ylim=c(-0,0.75))
    title(ylab = expression(PM[2.5]),
          main = expression("Total "*PM[2.5]*" Emission by Year"))
 
