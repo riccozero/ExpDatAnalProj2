fb2448e266a932e635a3f0ee1a36b831b640aaa8
riccozero <riccozero@gmail.com>
2014-05-19 22:33:16 -0500

    Question 2 Revision 3
    Using subset function instead

--- plot2.R
+++ plot2.R
@@ -1,14 +1,14 @@
 NEI <- readRDS("summarySCC_PM25.rds")
-NEI <- transform(NEI, year=as.factor(year))
-
-
 
 ### Question 2
 
-NEIBaltimore <- NEI[NEI$fip=="24510",]
-png("plot2.png", width=640, height=640)
+NEIBaltimore <- subset(NEI, fips=="24510")
 
-   boxplot(Emissions ~ year, data=NEIBaltimore, ylim=c(-0,2))
+NEIBaltimore <- transform(NEIBaltimore, year=as.factor(year))
+
+png("plot2.png", width=640, height=480)
+
+   boxplot(Emissions ~ year, data=NEIBaltimore, ylim=c(0,2))
    title(ylab = expression(PM[2.5]),
          main = expression("Total "*PM[2.5]*" Emission by Year in Baltimore"))
 
